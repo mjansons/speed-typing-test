90f5ecf387d34c97ab4de95abc991a5611859d51
Matiss Jansons <matiss93jansons@gmail.com>
2024-03-11 20:17:49 +0100

    moved forward and made some bugs

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function addWordsToHTML(wordList) {
         word.split('').forEach((char) => {
             let span = document.createElement('span');
             span.textContent = char;
+            span.className = `char`;
             wordElement.appendChild(span);
         });
 
@@ -37,59 +38,82 @@ let allWords = document.querySelectorAll(`.word`)
 let allChars = allWords[currentWordIndex].children;
 
 
-// Function to move to the next character or word
-function moveToNextChar(character) {
-    // If the current character matches the pressed key, move to the next character
-    if (allWords[currentWordIndex].children[currentCharIndex].textContent === character) {
-        allChars = allWords[currentWordIndex].children;
-        allChars[currentCharIndex].classList.add(`correct`)
-        currentCharIndex++;
-    }else{
-        allChars = allWords[currentWordIndex].children;
-        allChars[currentCharIndex].classList.add(`wrong`)
-        currentCharIndex++;
-    }
-}
 
 // Listen for keydown events
 document.addEventListener('keydown', function(event) {
+    let currentWord = allWords[currentWordIndex]
+    let currentChar = allWords[currentWordIndex].children[currentCharIndex]
+
     if (event.code === `Space`) {
+        evaluateWord();
         currentWordIndex++;
         currentCharIndex = 0;
     }else if(event.code === `Backspace`){
-        console.log(`Before backspace Char Index: ${currentCharIndex}`)
+        let previousChar = currentWord.children[(currentCharIndex - 1)]
+        // if the fucking letter has a classname excess char!!!!
+        // debugger
+        if(currentCharIndex > 0 && previousChar.className == `excess-char`){
+            currentWord.children[(currentCharIndex - 1)].remove();
+            currentCharIndex --;
+        }else if(currentCharIndex === 0 && currentWordIndex > 0 && allWords[(currentWordIndex - 1)].className !== `correct-word`){
+            currentWordIndex--;
+            currentCharIndex = (currentWord.children.length) - 1;
+            currentChar.className = `char`;
+        }else if(allWords[currentWordIndex][currentCharIndex]){
 
-        if(currentCharIndex === 0){
-            // allow to move back if word index is more than 0 AND
-            // if previous word char classes contain at least one wrong
-            // then I will make current word index -- and
-            // set char index to last child and take class away
         }
-        if(allWords[currentWordIndex])
+
         // if char has class excess:
             // remove char
             // char index -1
-        // if char index is < 0:
+        else if(currentCharIndex > 0){
+            console.log(`this was triggered`)
+            currentCharIndex--
+            allWords[currentWordIndex].children[currentCharIndex].className = `char`;
+        }
             // then char index -1
 
-
-        currentCharIndex--
-        allChars[currentCharIndex].className = ``;
-
-
-        console.log(`After backspace Char Index: ${currentCharIndex}`)
     }else if(currentCharIndex < allWords[currentWordIndex].children.length) {
         moveToNextChar(event.key);
     }else{
         // excess character adding
         let excessChar = document.createElement('span');
         excessChar.textContent = event.key;
+
         allWords[currentWordIndex].appendChild(excessChar);
-        allChars[currentCharIndex].classList.add(`excess`)
+        allChars[currentCharIndex].className = `excess-char`
         currentCharIndex++;
 
     }
 });
+
+// Function to move to the next character or word
+function moveToNextChar(character) {
+    // if correct-char key pressed
+    if (allWords[currentWordIndex].children[currentCharIndex].textContent === character) {
+        allChars = allWords[currentWordIndex].children;
+        allChars[currentCharIndex].className = `correct-char`
+        currentCharIndex++;
+    }else{
+        allChars = allWords[currentWordIndex].children;
+        allChars[currentCharIndex].className = `wrong-char`
+        currentCharIndex++;
+    }
+}
+
+
+function evaluateWord(){
+    const letters = Array.from(allWords[currentWordIndex].children)
+    if(letters.some((item) => item.className == `wrong-char` || item.className == `excess-char` || item.className == `char`)){
+        allWords[currentWordIndex].className = `wrong-word`
+    }else{
+        allWords[currentWordIndex].className = `correct-word`
+    }
+}
+
+
+
+
 
 
 
@@ -104,7 +128,7 @@ document.addEventListener('keydown', function(event) {
 // BACKSPACE LOGIC:
     // if char index is 0;
         // allow to move back if word index is more than 0 AND
-        // if previous word char classes contain at least one wrong
+        // if previous word char classes contain at least one wrong-char
         // then I will make current word index -- and
         // set char index to last child and take class away
     // if char has class excess:
